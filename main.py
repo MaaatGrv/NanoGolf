6405ca9f398796d75683ddf477c9c2580a9cf26c
Mathis Gorvien <mathis.gorvien@gmail.com>
2022-06-10 11:14:14 +0200

    add T

--- main.py
+++ main.py
@@ -127,6 +127,64 @@ def main():
     # o = Text('3ETI', np.array([-0.5, -0.2], np.float32), np.array([0.5, 0.3], np.float32), vao, 2, programGUI_id, texture)
     # viewer.add_object(o)
 
+    collision_box=[]
+
+    m = Mesh.load_obj('ball2.obj')
+    m.normalize()
+    m.apply_matrix(pyrr.matrix44.create_from_scale([0.2, 0.2, 0.2, 1])) #changer la taille de la sphere
+    tr = Transformation3D()
+    tr.translation.y = 0.4
+    tr.translation.z = -5
+    tr.rotation_center.z = 0.2
+    texture = glutils.load_texture('ball_red.png')
+    o = Object3D(m.load_to_gpu(), m.get_nb_triangles(), program3d_id, texture, tr)
+    viewer.add_object(o)
+
+    m = Mesh.load_obj('wall.obj')
+    m.normalize()
+    m.apply_matrix(pyrr.matrix44.create_from_scale([2, 2, 2, 1])) #changer la taille de la sphere
+
+    for i in range(3):
+        tr = Transformation3D()
+        tr.translation.y = 0.2
+        tr.translation.z = -5
+        tr.rotation_center.z = 0.2
+        tr.translation.x = 4*i
+        wmin=np.amin(m.vertices,axis=0)[:3]
+        wmax=np.amax(m.vertices,axis=0)[:3]
+        collision_box.append([wmin[0],wmin[2],wmin[0]+4*i,wmin[2]-5]) #recuperer les valeurs min  de notre objet
+        collision_box.append([wmax[0],wmax[2],wmax[0]+4*i,wmax[2]-5]) #recuperer les valeurs max  de notre objet
+        texture = glutils.load_texture('bois.png')
+        o = Object3D(m.load_to_gpu(), m.get_nb_triangles(), program3d_id, texture, tr)
+        viewer.add_object(o)
+    viewer.collision(collision_box)
+    
+    m = Mesh.load_obj('hole_round.obj')
+    m.normalize()
+    m.apply_matrix(pyrr.matrix44.create_from_scale([2, 2, 2, 1])) #changer la taille de la sphere
+    tr = Transformation3D()
+    tr.translation.x = 12
+    tr.translation.y = 0.2
+    tr.translation.z = -5
+    tr.rotation_center.z = -2
+    texture = glutils.load_texture('bois.png')
+    o = Object3D(m.load_to_gpu(), m.get_nb_triangles(), program3d_id, texture, tr)
+    viewer.add_object(o)
+    
+    m = Mesh.load_obj('flag.obj')
+    m.normalize()
+    m.apply_matrix(pyrr.matrix44.create_from_scale([2, 2, 2, 1])) #changer la taille de la sphere
+    tr = Transformation3D()
+    tr.translation.x = 12
+    tr.translation.y = 2
+
+    tr.translation.z = -4.3
+    tr.rotation_center.z = -2
+    texture = glutils.load_texture('flag_blue.png')
+    o = Object3D(m.load_to_gpu(), m.get_nb_triangles(), program3d_id, texture, tr)
+    viewer.add_object(o)
+
+
     viewer.run()
 
 def menu():
